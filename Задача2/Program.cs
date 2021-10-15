5f529e3bd9aa226984e27492075eb2159f4d90b7
Марина Разумова <marina.razumova.19@yandex.ru>
2021-10-15 14:52:48 +0300

    Добавьте файлы проекта.

---
+++ Задача2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Часть 2. Осуществить ввод последовательности целых чисел и сравнить, что больше, количество положительных или количество отрицательных.
+             * Последовательность потенциально не ограничена, окончанием последовательности служит число 0. */
+            int sum1 = 0;
+            int sum2 = 1;
+            int chislo;
+            int i = 0;
+            int r = 0;
+            do
+            {
+                chislo = Convert.ToInt32(Console.ReadLine());
+                if (chislo > 0)
+                {
+                    sum1 += chislo;
+                    i++;
+                }
+                else
+                    if (chislo != 0)
+                {
+                    sum2 += chislo;
+                    r++;
+                }
+            }
+            while (chislo != 0);
+            if (i > r)
+                Console.WriteLine("Колличество положительных чисел больше колличества отрицательных: {0} >{1}", i, r);
+            else if (r > i)
+                Console.WriteLine("Колличество  отрицательных чисел больше колличества положительных: {0} >{1}", r, i);
+            else
+                Console.WriteLine("Колличество  отрицательных чисел равно  колличества положительных: {0} ={1}", r, i);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Задача3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Часть 3. Ввести положительные числа A, B, C. 
+             * 	На прямоугольнике размера A x B размещено максимально возможное количество квадратов со стороной C (без наложений). 
+             *  Найти количество квадратов, размещенных на прямоугольнике. Операции умножения и деления не использовать. */
+            int i = 0;
+            int r = 0;
+            Console.WriteLine("Введите число А");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число В");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число С");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+
+            while ((a - c) >= 0)
+            {
+                a = a - c;
+                i++;
+            }
+
+            while ((b - c) >= 0)
+            {
+                b = b - c;
+                r++;
+            }
+
+            Console.WriteLine("Колличество квадратов , размещенных на прямоугольнике равно ={0}", (i * r));
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
